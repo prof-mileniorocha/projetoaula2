179719cff6238805529cfc238fb934678835b034
prof-mileniorocha <127246074+prof-mileniorocha@users.noreply.github.com>
2023-03-28 22:02:51 -0300

    Update calculadoraimc.c
    Atualização 2

--- calculadoraimc.c
+++ calculadoraimc.c
@@ -4,35 +4,29 @@ int main() {
     
     float peso, altura;
     float resultado_imc;
-    char nome[50];
     
-    printf("Digite seu nome: \n");
-    scanf("%s", nome);
+    int finaliza_programa;
     
-    printf("Orientações: O peso e a altura deve ser digitado com o . ao inves da vígula, por exemplo 1.8 (Quando a pessoa tem 1 metro e 80 centímetos) | 71.50 (Quando a pessoa pesa 71,5kg) \n");    
-    printf("%s digite seu peso: \n", nome);
+    printf("Orientações: O peso e a altura deve ser digitado com o . ao invés da vírgula, \n");
+    printf("Por exemplo 1.8 (Quando a pessoa tem 1 metro e 80 centímetros) | 71.50 (Quando a pessoa pesa 71,5kg) \n");
+    printf("------------------------------------------------------------------------------------------------------- \n");
+    do {
+    	printf("Digite seu peso: \n");
     scanf("%f", &peso);
     
-    printf("%s digite sua altura: \n", nome);
+    printf("Digite sua altura: \n");
     scanf("%f", &altura);
 
     resultado_imc = peso / (altura * altura);
     
-    printf("Resultado IMC: %.2f \n", resultado_imc);
-    printf("%s ", nome);
-    if(resultado_imc < 18.5){
-        printf("você está abaixo do peso normal! \n");
-    } else if(resultado_imc >= 18.5 && resultado_imc <= 24.9){
-        printf("Você está no peso normal! \n");
-    } else if(resultado_imc >= 25 && resultado_imc <= 29.9){
-        printf("Vocês está levemente acima do peso! \n");
-    } else if(resultado_imc >= 30 && resultado_imc <= 34.9){
-        printf("Vocês está com obesidade grau I! \n");
-    } else if(resultado_imc >= 35 && resultado_imc <= 39.9){
-        printf("Vocês está com obesidade grau II! \n");
-    } else {
-        printf("Você está com obesidade grau III (CUIDADO)! \n");
-    }
+    printf("Seu IMC é de: %.2f \n", resultado_imc);
 
+    printf("Digite 1 para continuar ou 2 para sair\n");
+    scanf("%d", &finaliza_programa);
+  
+    } while (finaliza_programa==1);
+    printf ("Programa finalizado! \n");
+    printf ("Muito obrigado por utilizar o programa \n");
+    
     return 0;
 }
